974aa881066766d9a9bf64328b5452d7011f41bd
TheHabibi <celebialieren@gmail.com>
2023-02-22 19:00:29 +0300

    connect DB

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,21 @@
 const express = require("express");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+const colors = require("colors");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db")
 
 const app = express()
 
 dotenv.config();
 
+connectDB();
+
 app.get('/', (req,res) => {
     res.send("API is Running")
 })
 
-app.get('/api/chat', (req,res) => {
-    res.send(chats)
-})
-
-app.get('/api/chat/:id',(req,res) => {
-   // console.log(req.params.id);
-})
+app.use('/api/user',userRoutes)
 
 const PORT = process.env.PORT
 
-app.listen(PORT,console.log(`Server started on PORT ${PORT}`));
+app.listen(PORT,console.log(`Server started on PORT ${PORT}`.yellow.bold));
