0936e59f4fda0694208a532be58f22867f278fba
HMFive <yusufciftci@outlook.com.tr>
2023-02-25 17:34:16 +0300

    Updata Navbar.js

--- frontend/src/shared/Navbar.js
+++ frontend/src/shared/Navbar.js
@@ -7,7 +7,7 @@ export function Navbar() {
             <div className="container flex flex-wrap items-center justify-between mx-auto">
                 <a href="dashboard" className="flex items-center">
                     <img src={logo} className="h-6 mr-3 sm:h-10"
-                         alt="Flowbite Logo"/>
+                         alt="Donut Share Logo"/>
                 </a>
                 <button data-collapse-toggle="navbar-solid-bg" type="button"
                         className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
