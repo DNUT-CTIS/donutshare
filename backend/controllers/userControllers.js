3afb93f0333e937a20e684785fc8a5f62608e0ed
TheHabibi <celebialieren@gmail.com>
2023-02-27 00:48:01 +0300

    Update userControllers.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -28,7 +28,7 @@ const registerUser = asyncHandler(async(req,res ) => {
    if(user) {
     res.status(201).json({
         _id: user._id,
-        username: user.name,
+        username: user.username,
         email: user.email,
         token:generateToken(user._id)
     });
