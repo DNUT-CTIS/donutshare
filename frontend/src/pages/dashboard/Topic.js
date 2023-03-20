e06d7a780c2822e4cc24607f58e1b6a06235aa3f
Onder172 <68420278+Onder172@users.noreply.github.com>
2023-03-20 16:31:22 +0300

    topic box updtade

--- frontend/src/pages/dashboard/Topic.js
+++ frontend/src/pages/dashboard/Topic.js
@@ -4,10 +4,10 @@ export function Topic() {
 
     return (
         <div class="flex flex-col text-center p-4 leading-normal  ">
-            <h1 class="mb-3 text-3xl font-bold text-gray-900 dark:text-white">Kemal Kılıçdaroğlu Can Win</h1>
+            <h1 class="mb-3 text-3xl font-bold text-gray-900 dark:text-white pt-6">Kemal Kılıçdaroğlu Can Win</h1>
 
 
-            <div class="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+            <div class="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4 p-8">
 
                 <div class="buttons">
                     <button>Agree</button>
