495c412a81abca6e48914e8f2cb6f365303249de
HMFive <yusufciftci@outlook.com.tr>
2023-03-20 16:34:43 +0300

    merge back and front

--- backend/controllers/reportControllers.js
+++ backend/controllers/reportControllers.js
@@ -20,24 +20,25 @@ const reportUser = asyncHandler(async(req,res) => {
     res.status(200).json(report)
 })
 
-const reportPost = asyncHandler(async(req,res)=>{
+const reportPost = asyncHandler(async(req,res)=> {
 
   console.log(req.body)
-    const post = await Post.findById(req.body.id);
-   
+  const post = await Post.findById(req.body.id);
+
 
   if (req.user.id == offender.id) {
     res.status(400);
     throw new Error("You can't report your own post!");
   }
 
-     const report = await Report.create({
-       complainant: req.user.username,
-       offender: user.username,
-       text: req.body.text,
-     });
+  const report = await Report.create({
+    complainant: req.user.username,
+    offender: user.username,
+    text: req.body.text,
+  });
 
-     res.status(200).json(report);
+  res.status(200).json(report);
+});
 
 const allReports = asyncHandler(async (req, res) => {
   const reportArr = await Report.find({});
