89b389b4742e9b5c25022160a4b1bdd47083e82d
TheHabibi <celebialieren@gmail.com>
2023-03-21 03:34:15 +0300

    Update timer.js

--- backend/config/timer.js
+++ backend/config/timer.js
@@ -1,10 +1,10 @@
 const Topic = require("../models/topicModel");
 
-const topicTimer = async() => {
+const topicTimer = () => {
 
-const topic = await Topic.findOneAndRemove().sort({ createdAt: 1 })
+const topic =  Topic.findOneAndRemove().sort({ createdAt: 1 })
 console.log(topic.content +" is deleted")
-const currentTopic = await Topic.findOne().sort({ createdAt: 1 }).limit(1);
+const currentTopic =  Topic.findOne().sort({ createdAt: 1 }).limit(1);
 currentTopic.isCurrent = true
 currentTopic.save()
 console.log(currentTopic.content + " is the new topic")
