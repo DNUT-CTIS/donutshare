e57f08efda904fbef31c736aff8c4407d79690e4
TheHabibi <celebialieren@gmail.com>
2023-03-21 03:23:16 +0300

    set to Turkey time zone

--- backend/controllers/topicControllers.js
+++ backend/controllers/topicControllers.js
@@ -21,7 +21,7 @@ const topic = await Topic.findOne({isCurrent: true})
  const now = new Date();
  const midnight = new Date();
  midnight.setHours(24, 0, 0, 0); // set to midnight tonight
- const timeLeft = midnight - now;
+ const timeLeft = midnight - now - 10800000;
  const secondsLeft = Math.floor((timeLeft / 1000) % 60);
  const minutesLeft = Math.floor((timeLeft / 1000 / 60) % 60);
  const hoursLeft = Math.floor((timeLeft / 1000 / 60 / 60) % 24);
