bd326e5bc6be4fca7640410d81d7a7404d7c721d
TheHabibi <celebialieren@gmail.com>
2023-03-22 07:40:44 +0300

    Update timer.js

--- backend/config/timer.js
+++ backend/config/timer.js
@@ -9,12 +9,7 @@ currentTopic.isCurrent = true
 currentTopic.save()
 console.log(currentTopic.content + " is the new topic")
 
-  const now = new Date();
-  const midnight = new Date();
-  midnight.setHours(24, 0, 0, 0);
-  const timeRemaining = midnight - now - 10800000;
-
-  setTimeout(topicTimer, timeRemaining);
+  setTimeout(topicTimer, 86400000);
 };
 
 module.exports = topicTimer;
