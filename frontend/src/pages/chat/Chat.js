1c84aaf390b9811e74663401ef79e1b4bbbb576d
HMFive <yusufciftci@outlook.com.tr>
2023-05-07 21:34:42 +0300

    add voice chat

--- frontend/src/pages/chat/Chat.js
+++ frontend/src/pages/chat/Chat.js
@@ -10,46 +10,99 @@ import ModalContainer from "../../shared/ModalContainer";
 import {generateRandomAvatarOptions} from '../dashboard/randomAvatar';
 import {PostModal} from "./PostModal";
 import {useNavigate} from "react-router-dom";
-import {WarningModal} from "../../shared/WarningModal";
+import Peer from "peerjs";
+
+
+
 
 const Chat = ({ match }) => {
   const [messages, setMessages] = useState([]);
   const [inputValue, setInputValue] = useState("");
   const [user, setUser] = useState();
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isWarningOpen, setIsWarningOpen] = useState(false);
   const [opinion, setOpinion] = useState("");
   const navigate = useNavigate();
-
   const messagesEndRef = useRef(null)
+  const peer = new Peer()
+
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth", block: "start"});
   }
   useEffect(() => {
-
     socket.on("connect", () => {
       console.log("this is chat");
       console.log(`Connected to server with ID ${socket.id}`);
     });
 
+    let audioConnection = null;
+    let audioStream = null;
+    let destPeer = null;
+    let call = null;
+
+    peer.on("open", (peerId) => {
+      console.log(`Connected to peer server with ID ${peerId}`);
+      socket.emit("peer-connection", peer.id);
+    });
+
+    socket.on("peer-bond", async (destPeerId) => {
+      console.log("opponents peer id: " + destPeerId);
+
+      audioConnection = peer.connect(destPeerId);
+      audioConnection.on("open", async () => {
+        console.log("Connected to remote-peer-id!");
+
+        // Save the destination peer ID
+        destPeer = destPeerId;
+      });
+    });
+
+    const pushToTalkButton = document.getElementById("push-to-talk");
+
+    pushToTalkButton.addEventListener("click", async () => {
+      // Get the user's audio stream
+      audioStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+      // Create a new PeerJS MediaConnection for the audio stream
+      call = peer.call(destPeer, audioStream);
+
+      // Play the audio stream from the other peer when it's received
+      call.on("stream", (stream) => {
+        const audioElement = document.createElement("audio");
+        audioElement.srcObject = stream;
+        audioElement.play();
+      });
+    });
+
+    peer.on("call", function (incomingCall) {
+      // Answer the call, providing our mediaStream
+      incomingCall.answer(audioStream);
+
+      // Enable audio track
+      audioStream.getAudioTracks()[0].enabled = true;
+
+      // Play the audio stream from the other peer when it's received
+      incomingCall.on("stream", (stream) => {
+        const audioElement = document.createElement("audio");
+        audioElement.srcObject = stream;
+        audioElement.play();
+      });
+    });
+
     socket.on("chatMessage", (message) => {
       setMessages((messages) => [...messages, message]);
-
     });
 
     socket.on("leaveChat", () => {
       console.log("Disconnected from server");
     });
-    scrollToBottom()
+    scrollToBottom();
 
     return () => {
       socket.off("connect");
       socket.off("chatMessage");
       socket.off("leaveChat");
     };
-
-
   }, [messages]);
 
   const handleInputChange = useCallback((event) => {
@@ -65,17 +118,9 @@ const Chat = ({ match }) => {
     navigate("/dashboard");
   };
 
-  const handleWarningClick = () => {
+  const handleWithdrawClick = () => {
     socket.emit("withdrawChat");
     setIsModalOpen(true);
-  }
-
-  const handleCancelClick = () => {
-    setIsWarningOpen(false);
-  }
-
-  const handleWithdrawClick = () => {
-    setIsWarningOpen(true);
   };
 
   const handleSendMessage = (event) => {
@@ -97,17 +142,6 @@ const Chat = ({ match }) => {
 
   return (
     <div>
-      <ModalContainer isOpen={isWarningOpen}>
-        <WarningModal></WarningModal>
-        <button onClick={handleWarningClick} data-modal-hide="popup-modal" type="button"
-                className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-          Yes, I'm sure
-        </button>
-        <button onClick={handleCancelClick} data-modal-hide="popup-modal" type="button"
-                className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No,
-          cancel
-        </button>
-      </ModalContainer>
       <ModalContainer isOpen={isModalOpen}>
         {console.log(opinion)}
         <PostModal side={opinion}></PostModal>
@@ -116,33 +150,44 @@ const Chat = ({ match }) => {
 
       <div className="flex flex-row dark:bg-zinc-700">
         <div className="dark:bg-zinc-800 h-screen flex flex-col w-2/4 dark:text-white">
-          <div className="text-center text-xl font-extrabold my-6">Your Match</div>
+          <div className="text-center text-xl font-extrabold my-6">
+            Your Match
+          </div>
           <div className="mx-auto text-center">
-            <Avatar className="rounded-full dark:bg-zinc-700 my-6 ml-10"
-                    {...avatarOptions}></Avatar>
+            <Avatar
+              className="rounded-full dark:bg-zinc-700 my-6 ml-10"
+              {...avatarOptions}
+            ></Avatar>
             {randomName}
-            <button className="bg-pink-600 text-black active:bg-pink-800
-        font-bold my-6 px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-32 mb-1" onClick={handleWithdrawClick}>Withdraw</button>
-            <button className="bg-pink-600 text-black active:bg-pink-800
-        font-bold px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mb-1">Report</button>
+            <button
+              className="bg-pink-600 text-black active:bg-pink-800
+        font-bold my-6 px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-32 mb-1"
+              onClick={handleWithdrawClick}
+            >
+              Withdraw
+            </button>
+            <button
+              className="bg-pink-600 text-black active:bg-pink-800
+        font-bold px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mb-1"
+            >
+              Report
+            </button>
           </div>
           <div className="text-center mx-auto my-16">
             <p className="font-extrabold text-xl">Todays donut</p>
             <h2>
               Burası contexlen passlanır heralde
-              <img src={donutStatic} alt=""/>
-
+              <img src={donutStatic} alt="" />
             </h2>
           </div>
-
         </div>
-        <div className="bg-white dark:bg-zinc-900 w-screen h-screen flex flex-col"
-             style={{ backgroundImage: `url(${donutBackground})` }}
+        <div
+          className="bg-white dark:bg-zinc-900 w-screen h-screen flex flex-col"
+          style={{ backgroundImage: `url(${donutBackground})` }}
         >
-          <div className="flex-1 overflow-y-scroll p-4"
-          >
+          <div className="flex-1 overflow-y-scroll p-4">
             {messages.map((message, index) => {
-              const username = JSON.parse(localStorage.getItem("username"))
+              const username = JSON.parse(localStorage.getItem("username"));
 
               const isSentByCurrentUser = username === message.username;
               console.log(isSentByCurrentUser);
@@ -156,24 +201,26 @@ const Chat = ({ match }) => {
               });
 
               return (
-                <div key={index} className="flex"
-                >
-                  <div className={messageClass}    ref={messagesEndRef}>
+                <div key={index} className="flex">
+                  <div className={messageClass} ref={messagesEndRef}>
                     <p>{message.message}</p>
                   </div>
                 </div>
               );
             })}
           </div>
-          <form onSubmit={handleSendMessage} className="flex dark:bg-zinc-800 items-center p-4">
+          <form
+            onSubmit={handleSendMessage}
+            className="flex dark:bg-zinc-800 items-center p-4"
+          >
             <input
-              maxLength={150}
               type="text"
               value={inputValue}
               onChange={handleInputChange}
               placeholder="Type a message"
               className="flex-1 rounded-full dark:bg-zinc-700 dark:text-white dark:focus:border-pink-700 border-gray-600 px-4 py-2 mr-2"
             />
+            <button id="push-to-talk">voice</button>
             <button
               type="submit"
               className="bg-pink-500 rounded-full text-white font-medium px-4 py-2"
@@ -183,8 +230,6 @@ const Chat = ({ match }) => {
           </form>
         </div>
       </div>
-
-
     </div>
   );
 };
